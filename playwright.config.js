b8eed82396679250f1fb6b4ae70a7aab34a174ad
HenrikLeutscher <HenrikHezo@hotmail.com>
2025-10-17 14:18:57 +0200

    Playwright configured properly

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig, devices } from "@playwright/test";
+require("dotenv").config();
 
 /**
  * Read environment variables from file.
@@ -28,6 +29,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
+    baseURL: "http://127.0.0.1:5500",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -72,9 +74,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: "npm run start",
+    url: "http://localhost:5500",
+    reuseExistingServer: !process.env.CI,
+  },
 });
